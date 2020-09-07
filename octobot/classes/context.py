0f164a4c5f22e7fad80292f709bbd5f3844c55fe
Yan <nezd@protonmail.com>
2020-09-07 17:38:49 +0300

    Dont use shlex split

--- octobot/classes/context.py
+++ octobot/classes/context.py
@@ -194,7 +194,7 @@ class Context:
             self.text = ''
         logger.debug("update type for id %s is %s", self.update.update_id, self.update_type)
         self.query = " ".join(self.text.split(" ")[1:])
-        self.args = shlex.split(self.query)
+        self.args = self.query.split(" ")
 
     @pluginfo_kwargs("reply_kwargs")
     def reply(self, text, photo_url=None, reply_to_previous=False, reply_markup=None, parse_mode=None, no_preview=False,
