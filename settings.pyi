507a486dbeedf9bc71d9ed43ef9b999e05c58e60
handlerug <umar@handlerug.me>
2021-04-23 15:09:50 +0000

    Update settings stub

--- settings.pyi
+++ settings.pyi
@@ -1,55 +1,56 @@
 from typing import Any
 
-LOGGER = ...  # type: Any
-FOLDER = ...  # type: Any
-Settings = ...  # type: _Settings
+LOGGER: Any
+FOLDER: Any
+Settings: _Settings
 
 
 class _Settings:
-    _settings = ...  # type: Any
-    telegram_token = ...  # type: str
-    production = ...  # type: bool
-    owner = ...  # type: int
-    telegram_base_url = ...  # type: str
-    telegram_base_file_url = ...  # type: str
-    telegram_base_file_url_force = ...  # type: bool
-    threads = ...  # type: int
-    exclude_plugins = ...  # type: list
-    support_url = ...  # type: str
-    user_agent = ...  # type: str
-    mozamibque_here_token = ...  # type: str
-    currency_converter_apikey = ...  # type: str
-    imgur_clientid = ...  # type: str
-    allowed_chats = ...  # type: list
-    disallowed_chat_reason = ...  # type: str
-    no_image = ...  # type: str
-    sentry = ...  # type: dict_sentry
-    redis = ...  # type: dict_redis
-    spamwatch = ...  # type: dict_spamwatch
+    _settings: Any
+    telegram_token: str
+    production: bool
+    owner: int
+    telegram_base_url: str
+    telegram_base_file_url: str
+    telegram_base_file_url_force: bool
+    threads: int
+    exclude_plugins: list
+    support_url: str
+    user_agent: str
+    mozamibque_here_token: str
+    currency_converter_apikey: str
+    imgur_clientid: str
+    allowed_chats: list
+    disallowed_chat_reason: str
+    no_image: str
+    sentry: dict_sentry
+    redis: dict_redis
+    spamwatch: dict_spamwatch
     def __init__(self) -> Any: ...
     def reload_settings(self) -> Any: ...
+    def update_settings(settings: Any) -> Any: ...
     def __getattr__(self, item: Any) -> Any: ...
     def get(self, item: Any) -> Any: ...
     def __setitem__(self, key: Any, value: Any) -> Any: ...
     def save_settings_to_disk(self) -> Any: ...
 
 class dotdict(dict):
-    __getattr__ = ...  # type: Any
-    __setattr__ = ...  # type: Any
-    __delattr__ = ...  # type: Any
+    __getattr__: Any
+    __setattr__: Any
+    __delattr__: Any
 
 class dict_sentry(dotdict):
-    enabled = ...  # type: bool
-    dsn = ...  # type: str
-    organization_slug = ...  # type: str
-    project_slug = ...  # type: str
+    enabled: bool
+    dsn: str
+    organization_slug: str
+    project_slug: str
 
 class dict_redis(dotdict):
-    host = ...  # type: str
-    port = ...  # type: int
-    db = ...  # type: int
+    host: str
+    port: int
+    db: int
 
 class dict_spamwatch(dotdict):
-    api_host = ...  # type: str
-    token = ...  # type: str
-    default_action = ...  # type: str
+    api_host: str
+    token: str
+    default_action: str
